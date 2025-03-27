452018454780fdcdc9645f8fbcc13cf89f75e22a
JOAQUINPIÑEYRO <73PI43442478@campus.economicas.uba.ar>
2025-03-26 22:07:57 -0300

    rollback

--- Facultad/Facultad/Form1.cs
+++ Facultad/Facultad/Form1.cs
@@ -16,21 +16,5 @@ namespace Facultad
         {
             InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtNombre_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnSaludar_Click(object sender, EventArgs e)
-        {
-            String nombre = txtNombre.Text;
-            MessageBox.Show("Hola" + nombre);
-        }
     }
 }
